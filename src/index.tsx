de0582a12bf231315048f9d4b6cc32f0d9076fbb
Andrew Newstead <andrew@doodlecube.co.uk>
2022-08-11 11:37:03 +0100

    testing branch CI/CD deploy test on github/netlify

--- src/index.tsx
+++ src/index.tsx
@@ -20,6 +20,8 @@ root.render(
   </React.StrictMode>
 );
 
+//just adding a non breaking change to git to test branch CI/CD deploy test on github/netlify
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
