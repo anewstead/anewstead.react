415160728939c1292b6f53a36d7ae6dfa209f743
Andrew Newstead <andrew@doodlecube.co.uk>
2022-09-24 17:27:33 +0100

    add basic Video test

---
+++ src/components/video/Video.test.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Video from "./Video";
+import { BASE_CONTENT_URL, BASE_VIDEO_URL } from "../../app/const";
+
+const videoURL = `${BASE_VIDEO_URL}1GMvNvQmYZI1AM8GRXR452LFvsebsz0JC`;
+const posterURL = `${BASE_CONTENT_URL}seat_leon.jpg`;
+
+test("renders TextBlock with good HMTL", async () => {
+  const { baseElement } = render(
+    <Video videoURL={videoURL} posterURL={posterURL} />
+  );
+  // eslint-disable-next-line testing-library/no-node-access
+  const video = baseElement.querySelector("video");
+  expect(video).toBeInTheDocument();
+});
