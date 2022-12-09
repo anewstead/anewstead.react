a5618dc5f02c9386f3a3c9b1bdc8913d077e249c
Andrew Newstead <>
2022-12-09 18:45:30 +0000

    storybook  dark theme and a11y addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,12 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/**/*.stories.@(mdx|js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/preset-create-react-app",
+    "@storybook/addon-a11y",
+    "storybook-dark-mode",
   ],
   framework: "@storybook/react",
   core: {
