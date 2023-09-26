42fab0ec86a6ba26369661131b5aef0dde0434bb
Andrew Newstead <>
2023-09-26 17:03:08 +0100

    update codegen format and add codegen for mockdata

--- codegen.ts
+++ codegen.ts
@@ -4,14 +4,30 @@ const config: CodegenConfig = {
   overwrite: true,
   schema:
     "https://api-eu-west-2.hygraph.com/v2/clj5rnzhr0grm01ui4kzcbmu1/master",
-  documents: "./src/services/hygraph/*.*",
+  documents: "./src/services/hygraph/*.gql",
+  ignoreNoDocuments: true,
   generates: {
-    "./src/services/hygraph/generated/": {
-      preset: "client",
-      plugins: [],
+    "./src/services/hygraph/generated/graphql.ts": {
+      plugins: ["typescript", "typescript-operations"],
+      config: {
+        useTypeImports: true,
+        skipTypename: true,
+        inlineFragmentTypes: "combine",
+      },
     },
     "./src/services/hygraph/generated/graphql.schema.json": {
       plugins: ["introspection"],
+    },
+    "./src/services/hygraph/generated/mockData.ts": {
+      plugins: [
+        {
+          "typescript-mock-data": {
+            typesFile: "./graphql.ts",
+            transformUnderscore: false,
+            terminateCircularRelationships: true,
+          },
+        },
+      ],
     },
   },
 };
