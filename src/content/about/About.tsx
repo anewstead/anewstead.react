fb00c44fd43457a07055d9e3bff7f8696d700fed
Andrew Newstead <>
2023-09-27 17:21:52 +0100

    about page cms

--- src/content/about/About.tsx
+++ src/content/about/About.tsx
@@ -1,62 +1,30 @@
 import React from "react";
 
-import { Box, Container, Paper, Typography } from "@mui/material";
+import { Container, Paper, Typography } from "@mui/material";
+import DOMPurify from "dompurify";
+import Markdown from "markdown-to-jsx";
 
 import { BRAND } from "../../const";
 import PageLayout from "../../layout/page-layout";
+import { useAppSelector } from "../../state/store";
 
 import cls from "./about.module.scss";
 
+import type { AppState } from "../../state/store";
+
 const About = () => {
+  const pageData = useAppSelector((state: AppState) => {
+    return state.mainData.data!.page;
+  });
+
   return (
     <PageLayout headerNavType="detail" headerNavTitle={BRAND}>
       <Container className={cls.about} data-testid="about-page">
         <Paper className={cls.paper}>
           <Typography variant="h4" component="h2">
-            About
+            {pageData?.title}
           </Typography>
-
-          <Typography
-            variant="body2"
-            gutterBottom
-            component={Box}
-            align="justify"
-          >
-            <p>Frontend/UI/UX developer</p>
-            <p>
-              This site shows some of the productions I have been involved with
-              over the years.
-            </p>
-            <p>
-              It also serves to show some non-client code, which in brief is
-              React and Typescript. <br />
-            </p>
-            <p>
-              It uses Material-UI components, basically because I wanted to try
-              it out.
-              <br />
-              In client work all components and CSS have almost always been
-              created bespoke from scratch.
-            </p>
-            <p>You can get all your tech insights here:</p>
-            <p>
-              <a
-                href="https://github.com/anewstead/anewstead.cra-mui"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Git: SPA React
-              </a>
-              <br />
-              <a
-                href="https://github.com/anewstead/anewstead.nextjs"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Git: SSR NextJS
-              </a>
-            </p>
-          </Typography>
+          <Markdown>{DOMPurify.sanitize(pageData!.info1)}</Markdown>
         </Paper>
       </Container>
     </PageLayout>
