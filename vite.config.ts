3340a5c314fd5ad2acf0dfddb53fc76a4cce0be6
Andrew Newstead <>
2023-12-14 13:54:38 +0000

    workaround (temp?) for vite reporting mui sourcemap error messages in storybook prod build,  minor package updates vite, mui, msw

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,22 @@ import checker from "vite-plugin-checker";
 import codegen from "vite-plugin-graphql-codegen";
 import { vitePluginGraphqlLoader } from "vite-plugin-graphql-loader";
 
+import { muteWarningsPlugin } from "./vite-mute-warnings-plugin";
+
+/**
+ * MuteWarningsPlugin / warningsToIgnore\
+ *
+ * SOURCEMAP_ERROR - noticed storybook build with vite 5 and mui components
+ * reporting sourcemap errors possibly due to "use client" flag\
+ * See: https://github.com/vitejs/vite/issues/15012
+ *
+ * TODO: check if resolved with newer releases of vite & plugin-react\
+ * Last checked vite 5.0.8
+ */
+const warningsToIgnore = [
+  ["SOURCEMAP_ERROR", "Can't resolve original location of error"],
+];
+
 const dev = () => {
   return {
     plugins: [
@@ -23,7 +39,11 @@ const dev = () => {
 
 const buildProd = () => {
   return {
-    plugins: [react(), vitePluginGraphqlLoader()],
+    plugins: [
+      react(),
+      vitePluginGraphqlLoader(),
+      muteWarningsPlugin(warningsToIgnore),
+    ],
     css: {
       devSourcemap: false,
     },
