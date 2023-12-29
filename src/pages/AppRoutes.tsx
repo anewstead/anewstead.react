41c27810a422e3df74cbf761e954ea4ab8e3db1f
Andrew Newstead <>
2023-12-29 00:49:37 +0000

    fix route

--- src/pages/AppRoutes.tsx
+++ src/pages/AppRoutes.tsx
@@ -16,7 +16,7 @@ export const router = createBrowserRouter([
     },
   },
   {
-    path: "about  ",
+    path: "about",
     async lazy() {
       const { About } = await import("../content/about");
       return { Component: About };
